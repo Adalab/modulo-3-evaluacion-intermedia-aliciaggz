ae3cc78c800c483159743377ba79ea6be4d43be4
Alicia Gallego Gonzalez <aliciagallegonz@gmail.com>
2022-01-18 09:29:49 +0100

    filter search

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,15 @@ import getData from '../services/api';
 
 function App() {
   const [data, setData] = useState([]);
+  const [newAdalaber, setNewAdalaber] = useState({
+    name: '',
+    tutor: '',
+    speciality: '',
+  });
 
+  const [search, setSearch] = useState('');
+
+  //API CON EFFECT
   useEffect(() => {
     getData().then((adalaber) => {
       setData(adalaber);
@@ -13,19 +21,56 @@ function App() {
 
   console.log(data);
 
+  //Handle
+
+  const handleSearchAdalaber = (ev) => {
+    setSearch(ev.currentTarget.value);
+  };
+
+  const handleChangeNewAdalaber = (ev) => {
+    setNewAdalaber({
+      ...newAdalaber,
+      [ev.currentTarget.id]: ev.currentTarget.value,
+    });
+  };
+
+  const handleClick = (ev) => {
+    ev.preventDefault();
+    setData([...data, newAdalaber]);
+
+    setNewAdalaber({
+      name: '',
+      tutor: '',
+      speciality: '',
+    });
+  };
+
   //paint table
 
-  const htmlAdalaber = data.map((adalaber) => (
-    <tr key={adalaber.id}>
-      <td>{adalaber.name}</td>
-      <td>{adalaber.tutor}</td>
-      <td>{adalaber.speciality}</td>
-    </tr>
-  ));
+  const htmlAdalaber = data
+    .filter((eachAdalaber) =>
+      eachAdalaber.name.toLowerCase().includes(search.toLowerCase())
+    )
+    .map((adalaber) => (
+      <tr key={adalaber.id}>
+        <td>{adalaber.name}</td>
+        <td>{adalaber.tutor}</td>
+        <td>{adalaber.speciality}</td>
+      </tr>
+    ));
 
   return (
     <div>
       <header>Adalabers</header>
+      <form>
+        <input
+          type="search"
+          name="search"
+          placerholder="Ej: Mari Carmen"
+          value={search}
+          onChange={handleSearchAdalaber}
+        />
+      </form>
       <table>
         <thead>
           <tr>
@@ -39,13 +84,37 @@ function App() {
 
       <div>
         <h2>Añadir una Adalaber</h2>
-        <label htmlFor="">Nombre:</label>
-        <input type="text" name="name" id="name" />
-        <label htmlFor="">Tutora</label>
-        <input type="text" name="tutor" id="tutor" />
-        <label htmlFor="">Especialidad</label>
-        <input type="text" name="speciality" id="speciality" />
-        <input type="submit" value="Añadir una nueva Adalaber" />
+        <form>
+          <label htmlFor="">Nombre:</label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            value={newAdalaber.name}
+            onChange={handleChangeNewAdalaber}
+          />
+          <label htmlFor="">Tutora</label>
+          <input
+            type="text"
+            name="tutor"
+            id="tutor"
+            value={newAdalaber.tutor}
+            onChange={handleChangeNewAdalaber}
+          />
+          <label htmlFor="">Especialidad</label>
+          <input
+            type="text"
+            name="speciality"
+            id="speciality"
+            value={newAdalaber.speciality}
+            onChange={handleChangeNewAdalaber}
+          />
+          <input
+            type="submit"
+            value="Añadir una nueva Adalaber"
+            onClick={handleClick}
+          />
+        </form>
       </div>
     </div>
   );
