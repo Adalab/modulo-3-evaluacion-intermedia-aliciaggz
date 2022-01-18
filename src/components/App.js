89addef220d4274a9c6c9eca61469b63a205cac0
Alicia Gallego Gonzalez <aliciagallegonz@gmail.com>
2022-01-18 09:11:41 +0100

    first part of eval

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,52 @@
 import '../styles/App.css';
 import { useEffect, useState } from 'react';
-import callToApi from '../services/api';
 import getData from '../services/api';
 
 function App() {
-  // Llamar al api con useEffect
+  const [data, setData] = useState([]);
 
   useEffect(() => {
-    getData().then((data) => {
-      setData(data);
+    getData().then((adalaber) => {
+      setData(adalaber);
     });
   }, []);
 
+  console.log(data);
+
+  //paint table
+
+  const htmlAdalaber = data.map((adalaber) => (
+    <tr key={adalaber.id}>
+      <td>{adalaber.name}</td>
+      <td>{adalaber.tutor}</td>
+      <td>{adalaber.speciality}</td>
+    </tr>
+  ));
+
   return (
     <div>
-      <h1>Hola Mundo</h1>
+      <header>Adalabers</header>
+      <table>
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Tutora</th>
+            <th>Especialidad</th>
+          </tr>
+        </thead>
+        <tbody>{htmlAdalaber}</tbody>
+      </table>
+
+      <div>
+        <h2>Añadir una Adalaber</h2>
+        <label htmlFor="">Nombre:</label>
+        <input type="text" name="name" id="name" />
+        <label htmlFor="">Tutora</label>
+        <input type="text" name="tutor" id="tutor" />
+        <label htmlFor="">Especialidad</label>
+        <input type="text" name="speciality" id="speciality" />
+        <input type="submit" value="Añadir una nueva Adalaber" />
+      </div>
     </div>
   );
 }
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,20 @@
 const getData = () => {
   return fetch(
     'https://beta.adalab.es/pw-recursos/apis/adalabers-v1/promo-patata.json'
-  ).then((response) => response.json());
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      const result = data.results.map((adalaber) => {
+        return {
+          id: adalaber.id,
+          name: adalaber.name,
+          tutor: adalaber.counselor,
+          speciality: adalaber.speciality,
+        };
+      });
+      // Retornamos los resultados del API al componente App
+      return result;
+    });
 };
 
 export default getData;
